5c701580557504df85687f57d72bc8487f005480
DorinVieru <dorino.vieru@gmail.com>
2024-05-27 11:45:09 +0200

    UP: Add function for create a Photo

--- net-il-mio-fotoalbum/Models/PhotoManager.cs
+++ net-il-mio-fotoalbum/Models/PhotoManager.cs
@@ -2,6 +2,28 @@
 {
     public class PhotoManager
     {
+        // AGGIUNTA DI UNA FOTO
+        public static void InsertPhoto(Photo Photo, List<string> SelectedCategories = null)
+        {
+            using PhotoContext db = new PhotoContext();
+
+            if (SelectedCategories != null)
+            {
+                Photo.Categories = new List<Category>();
+                // Trasformo gli ID scelti nel form in categorie da aggiungere alle foto
+                foreach (var catID in SelectedCategories)
+                {
+                    int id = int.Parse(catID);
+                    var cat = db.Categories.FirstOrDefault(i => i.Id == id);
+                    Photo.Categories.Add(cat);
+                }
+            }
+
+            db.Photos.Add(Photo);
+            db.SaveChanges();
+
+        }
+
         // PRENDERE TUTTE LE CATEGORIE DELLE FOTO
         public static List<Category> GetAllCategories()
         {
