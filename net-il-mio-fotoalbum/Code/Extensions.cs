2a5d7a9f1c2d3a3fec8889cd8c710bbcc8d24dc7
DorinVieru <dorino.vieru@gmail.com>
2024-05-27 16:01:59 +0200

    UP: Add function for shorten description string

---
+++ net-il-mio-fotoalbum/Code/Extensions.cs
@@ -0,0 +1,23 @@
+﻿namespace net_il_mio_fotoalbum.Code
+{
+    public static class Extensions
+    {
+        public static string SafeSubstring(this string input, int length, bool includeEllipsis = true)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            string ellipsis = "";
+            if (includeEllipsis && input.Length > length)
+            {
+                ellipsis = "...";
+                length -= ellipsis.Length;
+            }
+
+            if (length < 0) // Se la lunghezza è troppo piccola per includere i puntini
+                length = 0;
+
+            return input.Substring(0, Math.Min(length, input.Length)) + ellipsis;
+        }
+    }
+}
