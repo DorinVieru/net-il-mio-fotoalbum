5735bfdfe0a0b3ce5e00f4a9b91c8184532ac3df
DorinVieru <dorino.vieru@gmail.com>
2024-05-27 12:45:49 +0200

    UP: Add migrations for Photo, Categories and Identity, modify program and add roles

--- net-il-mio-fotoalbum/Models/PhotoContext.cs
+++ net-il-mio-fotoalbum/Models/PhotoContext.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace net_il_mio_fotoalbum.Models
 {
-    public class PhotoContext : DbContext
+    public class PhotoContext : IdentityDbContext<IdentityUser>
     {
         private const string SqlServer = "Data Source=localhost;Initial Catalog=photos;Integrated Security=True;Trust Server Certificate=True";
         public DbSet<Photo> Photos { get; set; }
