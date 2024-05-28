3dd33cbdd3747a246c6e50d8d0d8b34d6a016e24
DorinVieru <dorino.vieru@gmail.com>
2024-05-28 17:19:22 +0200

    UP: Add form message with custm design and imrove code of ContactMessage model

--- net-il-mio-fotoalbum/Models/ContactMessage.cs
+++ net-il-mio-fotoalbum/Models/ContactMessage.cs
@@ -1,10 +1,22 @@
-﻿namespace net_il_mio_fotoalbum.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit;
+using Xunit.Sdk;
+
+namespace net_il_mio_fotoalbum.Models
 {
     public class ContactMessage
     {
-        public int Id { get; set; }
+        [Key] public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Il nome può avere un massimo di 50 caratteri.")]
+        [Required(ErrorMessage = "Il nome è obbligatorio.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "La mail è obbligatoria.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Il messaggio è obbligatorio.")]
+        [MinLength(10)]
         public string Message { get; set; }
 
         public ContactMessage() { }
--- net-il-mio-fotoalbum/Models/Photo.cs
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -6,14 +6,14 @@ namespace net_il_mio_fotoalbum.Models
 {
     public class Photo
     {
-        [Key]  public int Id { get; set; }
+        [Key] public int Id { get; set; }
 
         [StringLength(50, ErrorMessage = "Il titolo può avere un massimo di 50 caratteri.")]
         [Required(ErrorMessage = "Il titolo è obbligatorio.")]
         public string Title { get; set; }
 
-        [StringLength(500, ErrorMessage = "Il nome della pizza può avere un massimo di 500 caratteri.")]
-        [Required(ErrorMessage = "La descrizione della pizza è obbligatoria.")]
+        [StringLength(500, ErrorMessage = "La descrizione della foto può avere un massimo di 500 caratteri.")]
+        [Required(ErrorMessage = "La descrizione della foto è obbligatoria.")]
         [MinLength(10)]
         public string Description { get; set; }
         public byte[]? ImgFile { get; set; }
