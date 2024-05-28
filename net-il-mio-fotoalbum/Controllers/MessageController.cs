37e1fb25f41b94c85b2824fa70ceb3bd17104560
DorinVieru <dorino.vieru@gmail.com>
2024-05-28 18:47:50 +0200

    UP: Improve code for vie categories on index home and for send email, add crud for view table email and delete

---
+++ net-il-mio-fotoalbum/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            var messages = ContactFormManager.GetAllMessages();
+            return View(messages);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var message = ContactFormManager.GetMessageById(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            return View(message);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            ContactFormManager.DeleteMessage(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- net-il-mio-fotoalbum/Models/ContactFormManager.cs
+++ net-il-mio-fotoalbum/Models/ContactFormManager.cs
@@ -1,13 +1,64 @@
-﻿namespace net_il_mio_fotoalbum.Models
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace net_il_mio_fotoalbum.Models
 {
     public class ContactFormManager
     {
+        // REGISTRAZIONE DI UN NUOVO MESSAGGIO INVIATO DALL'UTENTE NEL DATABASE
         public static void InsertMessage(ContactMessage message)
         {
             using PhotoContext db = new PhotoContext();
             
             db.Messages.Add(message);
             db.SaveChanges();
+        }
+
+        // OTTENERE LA LISTA DI TUTTE I MESSAGGI
+        public static List<ContactMessage> GetAllMessages()
+        {
+            using PhotoContext db = new PhotoContext();
+
+            return db.Messages.ToList();
+        }
+
+        // RECUPERARE UN MESSAGGIO TRAMITE IL SUO ID
+        public static ContactMessage GetMessageById(int id)
+        {
+            try
+            {
+                using PhotoContext db = new PhotoContext();
+
+                return db.Messages.FirstOrDefault(c => c.Id == id);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Errore: messaggio non trovato. -> {ex.Message}");
+                return null;
+            }
+        }
+
+        // CANCELLARE UNA CATEGORIA
+        public static bool DeleteMessage(int id)
+        {
+            try
+            {
+                var messageDelete = GetMessageById(id);
+                if (messageDelete == null)
+                    return false;
+
+                using PhotoContext db = new PhotoContext();
+                db.Remove(messageDelete);
+                db.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+
+            }
         }
     }
 }
