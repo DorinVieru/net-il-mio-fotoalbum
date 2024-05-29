7fe60c88170026d3ae285d31e19a0fccfa672320
DorinVieru <dorino.vieru@gmail.com>
2024-05-29 09:45:08 +0200

    BONUS: Edit program file for create different roles with different authorisation

--- net-il-mio-fotoalbum/Program.cs
+++ net-il-mio-fotoalbum/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using net_il_mio_fotoalbum.Models;
 using System.Text.Json.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
 namespace net_il_mio_fotoalbum
 {
     public class Program
@@ -44,7 +48,48 @@ namespace net_il_mio_fotoalbum
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
 
+            // Call the CreateRoles method to seed the roles and the SuperAdmin user.
+            CreateRoles(app).Wait();
+
             app.Run();
+        }
+
+        private static async Task CreateRoles(IHost app)
+        {
+            using var scope = app.Services.CreateScope();
+            var serviceProvider = scope.ServiceProvider;
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string[] roleNames = { "SuperAdmin", "Photographer", "User" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            var superAdmin = new IdentityUser
+            {
+                UserName = "superadmin",
+                Email = "superadmin@admin.com"
+            };
+
+            string superAdminPassword = "SuperAdminPass123!";
+            var _user = await userManager.FindByEmailAsync("superadmin@admin.com");
+
+            if (_user == null)
+            {
+                var createSuperAdmin = await userManager.CreateAsync(superAdmin, superAdminPassword);
+                if (createSuperAdmin.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(superAdmin, "SuperAdmin");
+                }
+            }
         }
     }
 }
